7dab2e569d0345b003d2d1f13e9de1f52fb36cb8
Greg Zelner <gzelner1@gmail.com>
2021-07-12 09:32:31 -0400

    Add project files.

---
+++ Random Number Generator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Random_Number_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int TestInt = 5;
+
+            Console.WriteLine("Hello we have {0} days to Armegeddon",TestInt);
+            //Random RandomNumber()=0;
+            //Console.WriteLine(RandomNumber);
+
+            Console.WriteLine("Hello World".Length);
+            string x= Console.ReadLine();
+            Console.WriteLine(x.Length);
+
+        }
+    }
+}
